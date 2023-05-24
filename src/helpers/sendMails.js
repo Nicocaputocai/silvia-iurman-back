666208284afc19af7a6e7219c0363030da120d03
adriangonzalez-dev <adriangonzalez.dev90@gmail.com>
2023-05-24 12:02:29 -0300

    emails

--- src/helpers/sendMails.js
+++ src/helpers/sendMails.js
@@ -1,54 +1,54 @@
-// const nodemailer = require('nodemailer')
+const nodemailer = require('nodemailer')
 
 // //PROBAR sendinblue https://es.sendinblue.com/
-// const transport = nodemailer.createTransport({
-//     host: process.env.HOST_MAIL,
-//     port: process.env.PORT_MAIL,
-//     auth: {
-//       user: process.env.USER_MAIL,
-//       pass: process.env.PASS_MAIL
-//     }
-//   });
+const transport = nodemailer.createTransport({
+    host: process.env.HOST_MAIL,
+    port: process.env.PORT_MAIL,
+    auth: {
+        user: process.env.USER_MAIL,
+        pass: process.env.PASS_MAIL
+    }
+});
 
-// module.exports={
-//     confirmRegister: async (data) =>{
-//         const {name, email, token} = data
-//         // console.log(data);
+module.exports = {
+    confirmRegister: async (data) => {
+        const { name, email, token } = data
+        console.log(data);
 
-//         try {
-//             const infoMail = await transport.sendMail({
-//                 from: "The Division Code",
-//                 to: email,
-//                 subject: "confirma tu cuenta",
-//                 text: "confirma tu cuenta proyect manager",
-//                 html: `
-//                 <p> Hola ${name}, para confirmar tu cuenta hacé click en el siguiente enlace</p>
-//                 <a href="${process.env.URL_FRONT}/confirm/${token}">Confirmá tu cuenta</a>
-//                 `
-//             })
-//         console.log(infoMail);
-//         } catch (error) {
-//             console.log(error);
-//         }
-//     },
-//     forgotPassword: async (data) =>{
-//         const {name, email, token} = data
-        
-//         // console.log(data);
-//         try {
-//             const infoMail = await transport.sendMail({
-//                 from: "The Division Code",
-//                 to: email,
-//                 subject: "reestablecé tu contaseña",
-//                 text: "reestablecé tu contraseña en  proyect manager",
-//                 html: `
-//                 <p> Hola ${name}, para reestablecer tu contraseña hacé click en el siguiente enlace</p>
-//                 <a href="${process.env.URL_FRONT}/recover-password/${token}">Confirmá tu cuenta</a>
-//                 `
-//             })
-//         console.log(infoMail);
-//         } catch (error) {
-//             console.log(error);
-//         }
-//     }
-// }
+        try {
+            const infoMail = await transport.sendMail({
+                from: "The Division Code",
+                to: email,
+                subject: "confirma tu cuenta",
+                text: "confirma tu cuenta proyect manager",
+                html: `
+<p> Hola ${name}, para confirmar tu cuenta hacé click en el siguiente enlace</p>
+<a href="${process.env.URL_FRONT}/confirm/${token}">Confirmá tu cuenta</a>
+`
+            })
+            console.log(infoMail);
+        } catch (error) {
+            console.log(error);
+        }
+    },
+    forgotPassword: async (data) => {
+        const { name, email, token } = data
+
+        console.log(data);
+        try {
+            const infoMail = await transport.sendMail({
+                from: "The Division Code",
+                to: email,
+                subject: "reestablecé tu contaseña",
+                text: "reestablecé tu contraseña en  proyect manager",
+                html: `
+<p> Hola ${name}, para reestablecer tu contraseña hacé click en el siguiente enlace</p>
+<a href="${process.env.URL_FRONT}/recover-password/${token}">Confirmá tu cuenta</a>
+`
+            })
+            console.log(infoMail);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
