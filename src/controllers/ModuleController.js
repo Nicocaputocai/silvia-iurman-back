feb0e24d819ee678168dd1410324a6f12ca005c6
nicocaputocai <nicolas_caputo@hotmail.com>
2023-06-05 19:46:18 -0300

    editar modulo andando

--- src/controllers/ModuleController.js
+++ src/controllers/ModuleController.js
@@ -17,16 +17,16 @@ module.exports = {
     },
     create:async(req,res)=>{
         const data = {
-            title:req.body.title,
-            typeModule:req.body.type,
-            link_intro:req.body.link_intro,
-            link:req.body.link,
-            date:req.body.date,
-            enabled:req.body.enabled,
-            open:req.body.open,
-            pricePesos:req.body.pricePesos,
-            priceAnticipedPesos:req.body.priceAnticipedPesos,
-            priceDolar:req.body.priceDolar,
+            title: req.body.title,
+            typeModule :req.body.type,
+            link_intro: req.body.link_intro,
+            link: req.body.link,
+            date: req.body.date,
+            enabled: req.body.enabled,
+            open: req.body.open,
+            pricePesos: req.body.pricePesos,
+            priceAnticipedPesos: req.body.priceAnticipedPesos,
+            priceDolar: req.body.priceDolar,
             priceAnticipedPesos: req.body.priceAnticipedPesos
         }
 
@@ -34,6 +34,32 @@ module.exports = {
         await newModule.save()
         return res.status(201).json({module:newModule});
 
+    },
+    update: async function(req,res){
+        console.log(req.body);
+        // let idModule = req.params._id;
+        let findModule = await Module.findById({_id: req.params._id});
+        if(!findModule){
+            return res.status(404).send({message:'El módulo no existe'})
+        }
+            const data= {
+                link_intro:req.body.link_intro,
+                link:req.body.link,
+                date:req.body.date,
+                enabled:req.body.enabled,
+                open:req.body.open,
+                pricePesos:req.body.pricePesos,
+                priceDolar:req.body.priceDolar,
+            };
+            await findModule.updateOne(data, {new:true},(err, moduleUpdated) =>{
+                if(err) return res.status(500).send({message:'Error en el servidor'})
+                if(moduleUpdated){
+                    return res.status(200).send({moduleUpdated})
+                }else{
+                    return res.status(404).send({message:'El módulo no existe'})
+                }
+            })
+        
     },
     delete: async (req,res)=>{
         const {id} = req.params;
@@ -48,5 +74,18 @@ module.exports = {
     deleteAll:async(req,res)=>{
         await Module.deleteMany();
         return res.status(200).json({message:'Modules deleted'});
+    },
+     //middleware para buscar cursos
+     find: function(req,res, next){ 
+        Module.find({_id: req.params._id})
+        .then(modulo => {
+            if(!modulo.length) return next();
+            req.body.modulo = modulo;
+            return next()
+        })
+        .catch(err => {
+            req.body.err = err ;
+            next();
+        })
     },
 }
--- src/routes/modules.js
+++ src/routes/modules.js
@@ -7,6 +7,7 @@ router.post('/create-all', ModulesController.createAll);
 router.get('/delete-all', ModulesController.deleteAll);
 router.get('/:id', ModulesController.getById);
 router.post('/create',ModulesController.create);
+router.put('/edit/:_id',  ModulesController.update);
 router.delete('/delete/:id', ModulesController.delete);
 
 module.exports = router
