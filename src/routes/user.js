d82406c86fb187cb5362f32fff6cf006c0ab3d5f
adriangonzalez-dev <adriangonzalez.dev90@gmail.com>
2023-06-20 13:05:38 -0300

    merging profile

--- src/routes/user.js
+++ src/routes/user.js
@@ -21,12 +21,8 @@ router
 .post('/login',loginValidation, getErrors, loginUser)
 .post('/register',registerValidation, getErrors, registerUser)
 .get('/relogged', checkUserLogged ,reloggedUser)
-<<<<<<< HEAD
-.put('/update-user', checkUserLogged, uploadImg.any(), updateUser)
-=======
 .put('/update-user', checkUserLogged, updateUser)
 .put('/update-avatar-user', checkUserLogged, uploadImage.single('avatar'), getErrors, updateProfileImage)
->>>>>>> profile
 .get('/confirm/:uuid', confirmAccount)
 .post('/recovery',sendTokenRecovery)
 .post('/recovery-password',recoveryPassword)
